be67a3556fbb75a0cfe6982d80c82116927a0f31
golivesid <155578467+golivesid@users.noreply.github.com>
2024-12-14 00:38:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ bot.on('message', async (msg) => {
         bot.sendMessage(chatId, `🔄 *Processing your link...*`).then(sentMessage => {
     const messageId = sentMessage.message_id;
 
-    axios.get(`https://opabhik.serv00.net/?link=${text}&apikey=0b010c132e2cbd862cbd8a6ae430dd51d3a0d5ea`)
+    axios.get(`https://opabhik.serv00.net/?url=${text}&apikey=30e048cd861b5524b81744d3461e78910593c3ba`)
         .then(response => {
             const downloadUrl = response.data.url;
 
