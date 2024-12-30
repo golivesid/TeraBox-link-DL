29a04292e99a73dc9466526efd04fa3f7f4b6b00
golivesid <155578467+golivesid@users.noreply.github.com>
2024-12-30 22:30:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ bot.on('message', async (msg) => {
         bot.sendMessage(chatId, `🔄 *Processing your link...*`).then(sentMessage => {
     const messageId = sentMessage.message_id;
 
-    axios.get(`https://www.terabox.tech/play.html?link=${text}&apikey=30e048cd861b5524b81744d3461e78910593c3ba`)
+    axios.get(`https://www.terabox.tech/play.html?url=${text}&apikey=30e048cd861b5524b81744d3461e78910593c3ba`)
         .then(response => {
             const downloadUrl = response.data.url;
 
